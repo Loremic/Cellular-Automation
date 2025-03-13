3b4be79d70cc1f7e4026a7b60527ae0e9a65ec88
Lor3mic <121642560+Loremic@users.noreply.github.com>
2025-03-13 21:08:48 +0100

    Add files via upload

---
+++ CA_1.py
@@ -0,0 +1,22 @@
+import Ca_0 as ca
+import time as tm
+
+ca.pg.init()
+
+r,t=True,0
+
+while r:
+    ca.sc.fill((0,0,0))
+    if t==0:
+        grid=ca.A
+    else:
+        grid=ca.ev(grid)
+    tm.sleep(0.25)
+    ca.draw(grid)
+    for e in ca.pg.event.get():
+        if e.type==ca.pg.QUIT:
+            r=False
+    ca.pg.display.update()
+    t+=1
+            
+ca.pg.quit()
